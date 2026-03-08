69a0e9f7ae617376736cd985079658d9e4fe8675
Humza A. Javaid <36482732+humzinator@users.noreply.github.com>
2026-03-08 19:32:03 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Humza Javaid";
+export const SITE_TITLE = "Humza A. Javaid";
 export const SITE_DESCRIPTION = "Welcome to my landing page!";
